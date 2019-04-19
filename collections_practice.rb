5481d06e549898163afc2cd747aeb9445808dced
wei feng <theweifeng@gmail.com>
2019-04-19 16:21:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,96 @@
-# your code goes here
+def begins_with_r(arr)
+  arr.all? {|ele| ele[0] == "r"}
+end
+
+def contain_a(arr)
+  new_arr = []
+  
+  arr.each {|ele| new_arr << ele if ele.include?(?a)}
+  
+  new_arr
+end
+
+def first_wa(arr)
+  arr.each {|ele| return ele if ele.to_s.include?("wa")}
+end
+
+def remove_non_strings(arr)
+  arr.select {|ele| ele.is_a?(String)}
+end
+
+def count_elements(arr)
+  new_arr = []
+  
+  arr.each do |ele|
+    ele[:count] = arr.count(ele)
+    new_arr << ele
+    arr.delete(ele)
+  end
+
+  new_arr
+end
+
+def merge_data(keys, data)
+  arr = []
+  
+    keys.each do |ele|
+      data.each do |ele2|
+        if ele2.has_key?(ele[:first_name])
+          arr << ele.merge(ele2[ele[:first_name]])
+        end
+    end
+  end
+  
+  arr
+end
+
+# let(:keys) {
+#     [
+#         {
+#             :first_name => "blake",
+#                 :motto => "Have a koala-ty day!"
+#         },
+#         {
+#             :first_name => "ashley",
+#                 :motto => "I dub thee, 'Lady Brett Ashley'."
+#         }
+#     ]
+#   }
+
+#   let(:data) {
+#     [
+#           {
+#             "blake" => {
+#                 :awesomeness => 10,
+#                     :height => "74",
+#                   :last_name => "johnson"
+#             },
+#             "ashley" => {
+#                 :awesomeness => 9,
+#                     :height => 60,
+#                   :last_name => "dubs"
+#             }
+#         }
+#     ]
+#   }
+
+#   let(:merged_data) {
+#     [
+#         {
+#             :first_name => "blake",
+#             :awesomeness => 10,
+#                 :height => "74",
+#               :last_name => "johnson",
+#                   :motto => "Have a koala-ty day!"
+
+#         },
+#         {
+#             :first_name => "ashley",
+#             :awesomeness => 9,
+#                 :height => 60,
+#               :last_name => "dubs",
+#                   :motto => "I dub thee, 'Lady Brett Ashley'."
+
+#         }
+#     ]
+#   }
