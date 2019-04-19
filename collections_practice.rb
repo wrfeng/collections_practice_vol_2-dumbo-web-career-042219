efb996378d4ddb790da8cd70e57b43ea0d7bda1e
wei feng <theweifeng@gmail.com>
2019-04-19 16:40:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -55,5 +55,43 @@ def find_cool(array)
 end
 
 def organize_schools(hash)
+  new_hash = []
   
+  hash.each do |key, val|
+    if new_hash[val[:location]].nil?
+      new_hash[val[:location]] = [key]
+    else
+      new_hash[val[:location]] << key
+    end
+  end
+  
+  new_hash
 end
+
+  let(:schools) {
+    {
+      "flatiron school bk" => {
+        :location => "NYC"
+      },
+      "flatiron school" => {
+        :location => "NYC"
+      },
+      "dev boot camp" => {
+        :location => "SF"
+      },
+      "dev boot camp chicago" => {
+        :location => "Chicago"
+      },
+      "general assembly" => {
+        :location => "NYC"
+      },
+      "Hack Reactor" => {
+        :location => "SF"
+      }
+    }
+  }
+  let(:organized_schools) {
+    {"NYC"=>["flatiron school bk", "flatiron school", "general assembly"],
+     "SF"=>["dev boot camp", "Hack Reactor"],
+     "Chicago"=>["dev boot camp chicago"]}
+  }
