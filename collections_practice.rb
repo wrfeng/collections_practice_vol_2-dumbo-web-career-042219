866f433175de200872635054b1bdd4dcc2fab980
wei feng <theweifeng@gmail.com>
2019-04-19 16:41:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -68,30 +68,30 @@ def organize_schools(hash)
   new_hash
 end
 
-  let(:schools) {
-    {
-      "flatiron school bk" => {
-        :location => "NYC"
-      },
-      "flatiron school" => {
-        :location => "NYC"
-      },
-      "dev boot camp" => {
-        :location => "SF"
-      },
-      "dev boot camp chicago" => {
-        :location => "Chicago"
-      },
-      "general assembly" => {
-        :location => "NYC"
-      },
-      "Hack Reactor" => {
-        :location => "SF"
-      }
-    }
-  }
-  let(:organized_schools) {
-    {"NYC"=>["flatiron school bk", "flatiron school", "general assembly"],
-     "SF"=>["dev boot camp", "Hack Reactor"],
-     "Chicago"=>["dev boot camp chicago"]}
-  }
+  # let(:schools) {
+  #   {
+  #     "flatiron school bk" => {
+  #       :location => "NYC"
+  #     },
+  #     "flatiron school" => {
+  #       :location => "NYC"
+  #     },
+  #     "dev boot camp" => {
+  #       :location => "SF"
+  #     },
+  #     "dev boot camp chicago" => {
+  #       :location => "Chicago"
+  #     },
+  #     "general assembly" => {
+  #       :location => "NYC"
+  #     },
+  #     "Hack Reactor" => {
+  #       :location => "SF"
+  #     }
+  #   }
+  # }
+  # let(:organized_schools) {
+  #   {"NYC"=>["flatiron school bk", "flatiron school", "general assembly"],
+  #   "SF"=>["dev boot camp", "Hack Reactor"],
+  #   "Chicago"=>["dev boot camp chicago"]}
+  # }
